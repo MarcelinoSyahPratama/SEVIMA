f93301700f720a7ed83652dcbe32b80e426988c7
Marcelino <marcell032017@gmail.com>
2022-06-25 12:48:49 +0700

    add modal

--- php/login.php
+++ php/login.php
@@ -40,7 +40,9 @@ $error = true;
 <!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="refresh" content="1" > 
+    <!-- <meta http-equiv="refresh" content="1" >  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 	<link rel="stylesheet" type="text/css" href="../css/login.css">
 	<title>login</title>
 </head>
@@ -74,11 +76,121 @@ $error = true;
 						<li>
 							<button type="submit" name="login">Login</button>
 						</li>
-						<li><a href="reg.php"><p style="float: left;color: red;">Belum punya akun?</p></a><p style="float: right;color: red;">forgot password</p></li>
+						<li><a type="button" data-bs-toggle="modal" data-bs-target="#regis"><p style="float: left;color: red;">Belum punya akun?</p></a><p style="float: right;color: red;">forgot password</p></li>
 					</ul>
 				</form>
 			</div>
 		</div>
 	</div>
+    <div class="modal fade" id="regis" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form action="" method="post">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Registrasi</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body" style="display: block;">
+                        
+                        <div class="input-group input-group-sm mb-3">
+                            <label for="">Nama</label>
+                            <input type="text" class="form-control" name="nama" aria-label="nama" aria-describedby="basic-addon1">
+                        </div>
+                        <div class="input-group input-group-sm mb-3">
+                            <label for="">Username</label>
+                            <input type="text" class="form-control" name="username" aria-label="Username" aria-describedby="basic-addon1">
+                        </div>
+                        <div class="input-group input-group-sm mb-3">
+                            <label for="">Password</label>
+                            <input type="text" class="form-control" name="pass" id="pass" aria-label="passwprd" aria-describedby="basic-addon1">
+                        </div>
+                        <div class="input-group input-group-sm mb-3">
+                            <label for="">Re-Password</label>
+                            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                            <input type="checkbox" onclick="viewpass()"> Lihat Password
+                        </div>
+                        
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- modal insert -->
+<!-- <div class="example-modal">
+  <div id="tambahuser" class="modal fade" role="dialog" style="display:none;">
+    <div class="modal-dialog"> 
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h3 class="modal-title">Registrasi User Baru</h3>
+        </div>
+        <div class="modal-body">
+          <form action="../user/function_user.php?act=tambahuser" method="post" role="form">
+            <div class="form-group">
+              <div class="row">
+              <label class="col-sm-3 control-label text-right">Id User <span class="text-red">*</span></label>         
+              <div class="col-sm-8"><input type="text" class="form-control" name="id_user" placeholder="Id User" value=""></div>
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="row">
+              <label class="col-sm-3 control-label text-right">Username <span class="text-red">*</span></label>
+              <div class="col-sm-8"><input type="text" class="form-control" name="username" placeholder="Username" value=""></div>
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="row">
+              <label class="col-sm-3 control-label text-right">Password <span class="text-red">*</span></label>
+              <div class="col-sm-8"><input type="password" class="form-control" name="password" placeholder="Password" id="myPassword" value="">
+              <input type="checkbox" onclick="myFunction()"> Lihat Password
+              <script>
+              function myFunction() {
+                var x = document.getElementById("myPassword");
+                if (x.type === "password") {
+                    x.type = "text";
+                } else {
+                    x.type = "password";
+                }
+              }
+              </script>
+              </div>
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="row">
+              <label class="col-sm-3 control-label text-right">User Role <span class="text-red">*</span></label>
+                <div class="col-sm-8"><select name="role" class="form-control select2" style="width: 100%;">
+                  <option value="User" selected="selected">-- Pilih Satu --</option>
+                  <option value="admin">Administrator</option>
+                  <option value="staff">Staff</option>
+                </select>
+                </div>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button id="nosave" type="button" class="btn btn-danger pull-left" data-dismiss="modal">Batal</button>
+              <input type="submit" name="submit" class="btn btn-primary" value="Simpan">
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>modal insert close -->
+<script>
+    function viewpass() {
+        var x = document.getElementById("Pass");
+        if (x.type === "password") {
+            x.type = "text";
+        } else {
+            x.type = "password";
+        }
+    }
+</script>
 </body>
 </html>
