a8f21f7cb5bce0a9af7c28d1e292351a1c8d3855
Marcelino <marcell032017@gmail.com>
2022-06-25 14:50:51 +0700

    add FE list kelas

---
+++ php/listKelas.php
@@ -0,0 +1,83 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>home</title>
+    <link rel="stylesheet" href="../css/semantic.min.css">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+    <script src="https://kit.fontawesome.com/62f37a0509.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <div class="pusher">
+      <div class="ui container p-t">
+        <div class="ui menu inverted navbar large blue">
+          <div class="left menu" style="font-size: 20px;">
+            <a class="item">
+              Selamat Belajar 
+            </a>
+            <a href="paket1.php" class="item">
+              Daftar Kelas
+            </a>
+            <a href="paket1.php" class="item">
+                Gabung Kelas
+              </a>
+            <a href="paket1.php" class="item">
+                Profile
+              </a>
+              <a href="paket1.php" class="item">
+                Daftar Tugas
+              </a>
+            <a href="logout.php" class="item">
+              Log out
+            </a>
+
+          </div>
+          <div class="header item fixed right menu">
+            <b><h3 style="font-size: 25px"><strong>SEDANG</strong></h3></b>
+          </div>
+
+        </div>
+
+        <div class="ui header">
+          <div class="heading2">Kelas</div>
+          <div class="ui clearing divider"></div>
+        </div>
+
+        <div class="ui stackable four column grid">
+          <div class="row">
+            <div class="column">
+              <div class="ui card">
+                <a class="image" href="" style="overflow: hidden;">
+                  <img src="https://gstatic.com/classroom/themes/img_read.jpg" style="width: 130%;height: 140px;">
+                </a>
+                <div class="content">
+                  <a class="header">XII RPL C</a>
+                  <div class="meta">Matematika</div>
+                  <div class="description">
+                    <p>from:<strong>Marcel</strong></p>
+                  </div>
+                </div>
+                <div class="extra content">
+                  <i class="fas fa-chalkboard-teacher"></i> &nbsp;&nbsp;&nbsp;Mulai Belajar
+                </div>
+              </div>
+              </div>
+              
+          </div>
+        </div>
+
+        
+
+
+      </div>
+
+    </div>
+
+
+    <script src="assest/semantic.min.js"></script>
+    <script src="assest/jquery.js"></script>
+    <script src="assest/package.js"></script>
+  </body>
+</html>
